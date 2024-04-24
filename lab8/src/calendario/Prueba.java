801b8593c5e966e1b6979b1e65f7fe68c7caa269
Mateo Card <110208091+Mate0521@users.noreply.github.com>
2024-04-24 13:59:39 -0500

    second commit

---
+++ lab8/src/calendario/Prueba.java
@@ -0,0 +1,16 @@
+package calendario;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Prueba {
+	
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		Calendar cal = Calendar.getInstance();
+	    cal.setTime(new Date(2024-1900, 3, 1));
+	    System.out.println(new Date(2024-1900, 3, 1));
+	    System.out.println(cal.get(Calendar.DAY_OF_WEEK));
+	}
+}
+
